b0e373138679b5009af25372e29f8402dacda605
Carlos Costa <ccosta@estgl.ipv.pt>
2022-04-14 11:13:29 +0100

    move App to a package

--- HelloWorld/src/cp/helloapp/App.java
+++ HelloWorld/src/cp/helloapp/App.java
@@ -1,5 +1,7 @@
+package cp.helloapp;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
